e58c8596d6434b8cb312fb4b346925daf51b5371
endijulian <endijulian080798@gmail.com>
2023-06-28 16:09:16 +0700

    slicing home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,13 +2,21 @@ import Header from "parts/Header";
 import React from "react";
 import Hero from "parts/Hero";
 import JustArrived from "parts/HomePage/JustArrived";
+import Browse from "parts/HomePage/Browse";
+import Clients from "parts/Clients";
+import Sitemap from "parts/HomePage/Sitemap";
+import Footer from "parts/Footer";
 
 export default function HomePage(props) {
   return (
     <>
       <Header></Header>
       <Hero></Hero>
+      <Browse></Browse>
       <JustArrived></JustArrived>
+      <Clients></Clients>
+      <Sitemap></Sitemap>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/parts/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer className="flex text-center px-4 py-8 justify-center">
+        <p className="text-sm">
+          Copyright 2021 • All Rights Reserved LuxSpace by BuildWith Angga
+        </p>
+      </footer>
+    );
+  }
+}
